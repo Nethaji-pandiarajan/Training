83870558ce9ad8c71275b0b33cd19aaad312be06
Nethaji-pandiarajan <82449478+Nethaji-pandiarajan@users.noreply.github.com>
2021-04-13 17:21:48 +0530

    Add files via upload

---
+++ subtraction.java
@@ -0,0 +1,11 @@
+
+public class subtraction {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=30;
+		b=20;
+		c=a-b;
+		System.out.println("The answer for subtraction is:" + c);
+	}
+}
