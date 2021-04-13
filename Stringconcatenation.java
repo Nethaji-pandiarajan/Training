93d25cb0a71d3eedcff4aeb2442de28c2aec3797
Nethaji-pandiarajan <82449478+Nethaji-pandiarajan@users.noreply.github.com>
2021-04-13 17:22:34 +0530

    Add files via upload

---
+++ Stringconcatenation.java
@@ -0,0 +1,11 @@
+
+public class Stringconcatenation {
+	
+	public static void main(String[] args) {
+		
+		String a= "Nethaji";
+		String b=" Rockzs";
+		String c= a + b;
+		System.out.println(" " + c);
+	}
+}
